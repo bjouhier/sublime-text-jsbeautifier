9018a22e0c83bc163a6eed90734d749c5ce072b1
Bruno Jouhier <bjouhier@gmail.com>
2011-10-25 22:44:09 +0200

    indent with tab rather than spaces

--- js_formatter.py
+++ js_formatter.py
@@ -3,8 +3,8 @@ import sublime, sublime_plugin, jsbeautifier
 class JsFormatCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		opts = jsbeautifier.default_options();
-		opts.indent_char = " "
-		opts.indent_size = 4
+		opts.indent_char = "\t"
+		opts.indent_size = 1
 		opts.max_preserve_newlines = 3
 		selection = self.view.sel()[0]
 		replaceRegion = selection if len(selection) > 0 else sublime.Region(0, self.view.size())
